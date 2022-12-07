62e5eb831fd70dee54bbbb2e399e47659bc1f7b4
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2022-12-07 16:58:28 +0100

    Wy

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -8,9 +8,11 @@ public class MyApp {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-
+        MessageService messageService1 = applicationContext.getBean("messageService", MessageService.class);
         System.out.println(messageService.getMessage());
+        System.out.println(messageService1.getMessage());
 
         applicationContext.close();
+
     }
 }
