8b61b3df505a46b6bdc042892702dc4e77d4c30f
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2022-12-07 16:56:02 +0100

    Wykonanie zadania 9.
    FIX:
    RandomTextMessageService.getMessage() losuje już poprawnie

--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -4,6 +4,6 @@ public class RandomTextMessageService implements MessageService{
     String[] messages = {"m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9"};
     @Override
     public String getMessage(){
-        return messages[(int) (Math.random() % 10)];
+        return messages[(int) Math.floor(Math.random() * 10)];
     }
 }
