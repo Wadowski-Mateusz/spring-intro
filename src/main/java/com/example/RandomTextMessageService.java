1d96f763067306fb970d435557c6b73663abb655
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2022-12-07 17:10:02 +0100

    Zadanie 15

--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import org.springframework.stereotype.Component;
+
+@Component("messageService1")
 public class RandomTextMessageService implements MessageService{
     String[] messages = {"m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9"};
     @Override
